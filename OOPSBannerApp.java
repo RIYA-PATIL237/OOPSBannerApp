87a1126178ffadb2021b63a8dff53af707e102cd
RIYA-PATIL237 <riyaspatil001@gmail.com>
2026-02-24 14:05:37 +0530

    UC6 implemented using static helper methods for O, P, S

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,6 +1,6 @@
 /**
  * OOPSBannerApp
- * Prints OOPS to console
+ * Prints OOPS using static helper methods
  * @author RP
  * @version 1.0
  */
@@ -9,27 +9,46 @@ public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        // Declare and initialize array in one step
-        String[] banner = {
+        String[] o = buildO();
+        String[] p = buildP();
+        String[] s = buildS();
 
-                String.join("",
-                        "*","*","*","*","*","*","*","*","*","*",
-                        "*","*","*","*","*","*","*","*","*","*"),
-
-                String.join("", "*", "                  ", "*"),
-
-                String.join("", "*", "      OOPS       ", "*"),
-
-                String.join("", "*", "                  ", "*"),
-
-                String.join("",
-                        "*","*","*","*","*","*","*","*","*","*",
-                        "*","*","*","*","*","*","*","*","*","*")
-        };
-
-        // Enhanced for loop
-        for (String line : banner) {
-            System.out.println(line);
+        // Loop row by row
+        for (int i = 0; i < o.length; i++) {
+            System.out.println(o[i] + "  " + o[i] + "  " + p[i] + "  " + s[i]);
         }
+    }
+
+    // Method to build O
+    public static String[] buildO() {
+        return new String[] {
+                " *** ",
+                "*   *",
+                "*   *",
+                "*   *",
+                " *** "
+        };
+    }
+
+    // Method to build P
+    public static String[] buildP() {
+        return new String[] {
+                "**** ",
+                "*   *",
+                "**** ",
+                "*    ",
+                "*    "
+        };
+    }
+
+    // Method to build S
+    public static String[] buildS() {
+        return new String[] {
+                " ****",
+                "*    ",
+                " *** ",
+                "    *",
+                "**** "
+        };
     }
 }
