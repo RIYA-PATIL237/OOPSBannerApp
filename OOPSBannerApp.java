a99d9eec5eedb116f6da7af5c91a682f0ad1197b
RIYA-PATIL237 <riyaspatil001@gmail.com>
2026-02-24 14:08:18 +0530

    UC7 implemented using static inner class CharacterPattern

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,54 +1,73 @@
 /**
  * OOPSBannerApp
- * Prints OOPS using static helper methods
- * @author RP
- * @version 1.0
+ * UC7 – Store Character Pattern in a Class
  */
 
 public class OOPSBannerApp {
 
-    public static void main(String[] args) {
+    // Static Inner Class
+    static class CharacterPattern {
 
-        String[] o = buildO();
-        String[] p = buildP();
-        String[] s = buildS();
+        private char character;
+        private String[] pattern;
 
-        // Loop row by row
-        for (int i = 0; i < o.length; i++) {
-            System.out.println(o[i] + "  " + o[i] + "  " + p[i] + "  " + s[i]);
+        // Constructor
+        public CharacterPattern(char character, String[] pattern) {
+            this.character = character;
+            this.pattern = pattern;
+        }
+
+        // Getter
+        public String[] getPattern() {
+            return pattern;
         }
     }
 
-    // Method to build O
-    public static String[] buildO() {
-        return new String[] {
+    public static void main(String[] args) {
+
+        CharacterPattern o = new CharacterPattern('O', new String[]{
                 " *** ",
                 "*   *",
                 "*   *",
                 "*   *",
                 " *** "
-        };
-    }
+        });
 
-    // Method to build P
-    public static String[] buildP() {
-        return new String[] {
+        CharacterPattern p = new CharacterPattern('P', new String[]{
                 "**** ",
                 "*   *",
                 "**** ",
                 "*    ",
                 "*    "
-        };
-    }
+        });
 
-    // Method to build S
-    public static String[] buildS() {
-        return new String[] {
+        CharacterPattern s = new CharacterPattern('S', new String[]{
                 " ****",
                 "*    ",
                 " *** ",
                 "    *",
                 "**** "
-        };
+        });
+
+        String word = "OOPS";
+
+        // Loop row by row
+        for (int i = 0; i < 5; i++) {
+
+            StringBuilder line = new StringBuilder();
+
+            for (char c : word.toCharArray()) {
+
+                if (c == 'O') {
+                    line.append(o.getPattern()[i]).append("  ");
+                } else if (c == 'P') {
+                    line.append(p.getPattern()[i]).append("  ");
+                } else if (c == 'S') {
+                    line.append(s.getPattern()[i]).append("  ");
+                }
+            }
+
+            System.out.println(line);
+        }
     }
 }
