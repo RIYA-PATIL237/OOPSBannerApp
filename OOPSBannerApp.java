fad4ef9fedeb8edc5129239568f735cf76baac38
RIYA-PATIL237 <riyaspatil001@gmail.com>
2026-02-24 13:58:51 +0530

    UC4 implemented using String array and loop

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -11,30 +11,29 @@ public class OOPSBannerApp {
         printBanner();
     }
 
-    // Static method to print full banner
     public static void printBanner() {
 
-        System.out.println(createTopBottom());
-        System.out.println(createEmptyLine());
-        System.out.println(createOOPSLine());
-        System.out.println(createEmptyLine());
-        System.out.println(createTopBottom());
-    }
-
-    // Creates top and bottom border
-    public static String createTopBottom() {
-        return String.join("",
+        // Create banner lines using String.join()
+        String topBottom = String.join("",
                 "*","*","*","*","*","*","*","*","*","*",
                 "*","*","*","*","*","*","*","*","*","*");
-    }
 
-    // Creates empty line with borders
-    public static String createEmptyLine() {
-        return String.join("", "*", "                  ", "*");
-    }
+        String emptyLine = String.join("", "*", "                  ", "*");
 
-    // Creates OOPS text line
-    public static String createOOPSLine() {
-        return String.join("", "*", "      OOPS       ", "*");
+        String oopsLine = String.join("", "*", "      OOPS       ", "*");
+
+        // Store all lines in an array
+        String[] banner = {
+                topBottom,
+                emptyLine,
+                oopsLine,
+                emptyLine,
+                topBottom
+        };
+
+        // Enhanced for-loop to print banner
+        for (String line : banner) {
+            System.out.println(line);
+        }
     }
 }
